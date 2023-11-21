90bdd79f7494a95b1902110bf4272d86bb2fff85
rodrigo.baggio.si@gmail.com <Baggio25>
2023-11-21 07:36:53 -0300

    Criado CategoryResource

--- backend/src/main/java/com/baggio/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/baggio/dscatalog/resources/CategoryResource.java
@@ -1,12 +1,27 @@
 package com.baggio.dscatalog.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.baggio.dscatalog.entities.Category;
+
 @RestController
-@RequestMapping(value = "categories")
+@RequestMapping(value = "/categories")
 public class CategoryResource {
 	
-	
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll() {
+		List<Category> list = new ArrayList<Category>();
+		list.add(new Category(1L, "Livros"));
+		list.add(new Category(2L, "Eletrônicos"));
+		list.add(new Category(1L, "Escolar"));
+		
+		return ResponseEntity.ok(list);
+	}
 
 }
