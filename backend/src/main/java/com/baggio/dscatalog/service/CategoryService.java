b5966e2d985d3d3fe6ec7678920b7c3a94766910
rodrigo.baggio.si@gmail.com <Baggio25>
2023-11-27 07:43:11 -0300

    Criado endpoint e service para FindById

--- backend/src/main/java/com/baggio/dscatalog/service/CategoryService.java
+++ backend/src/main/java/com/baggio/dscatalog/service/CategoryService.java
@@ -1,6 +1,7 @@
 package com.baggio.dscatalog.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,6 +20,11 @@ public class CategoryService {
 		List<Category> categories = categoryRepository.findAll(); 
 		return categories.stream().map(category -> new CategoryDTO(category)).toList();
 		
+	}
+	
+	public CategoryDTO findById(Long id) {
+		Optional<Category> categoryOpt =  categoryRepository.findById(id);
+		return new CategoryDTO(categoryOpt.get());
 	}
 	
 	
