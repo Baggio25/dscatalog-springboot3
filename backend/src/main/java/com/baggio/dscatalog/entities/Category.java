7f0b6c545e3d7997d0c7758c3c64aec96b11afdd
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2024-01-16 21:10:06 -0300

    ORM mapping e database seeding

--- backend/src/main/java/com/baggio/dscatalog/entities/Category.java
+++ backend/src/main/java/com/baggio/dscatalog/entities/Category.java
@@ -1,13 +1,16 @@
 package com.baggio.dscatalog.entities;
 
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -26,6 +29,9 @@ public class Category {
 
 	@Column(name = "updated_at", columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant updatedAt;
+	
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
 
@@ -60,6 +66,10 @@ public class Category {
 		return updatedAt;
 	}
 	
+	public Set<Product> getProducts() {
+		return products;
+	}
+
 	@PrePersist
 	public void prePersist() {
 		createdAt = Instant.now();
---
+++ backend/src/main/java/com/baggio/dscatalog/entities/Role.java
@@ -0,0 +1,73 @@
+package com.baggio.dscatalog.entities;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_role")
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String authority;
+	
+	@ManyToMany(mappedBy = "roles")
+	private Set<User> users = new HashSet<>();
+	
+	public Role() {
+
+	}
+
+	public Role(Long id, String authority) {
+		this.id = id;
+		this.authority = authority;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAuthority() {
+		return authority;
+	}
+
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+
+	public Set<User> getUsers() {
+		return users;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+
+}
