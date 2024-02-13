f4ee3b46bce51c4dad492f45fa973afcfaaf1c82
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2024-02-13 14:19:39 -0300

    UserUpdateValid p2

--- backend/src/main/java/com/baggio/dscatalog/services/validation/UserUpdateValidator.java
+++ backend/src/main/java/com/baggio/dscatalog/services/validation/UserUpdateValidator.java
@@ -2,18 +2,24 @@ package com.baggio.dscatalog.services.validation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
 
 import com.baggio.dscatalog.dto.UserUpdateDTO;
 import com.baggio.dscatalog.entities.User;
 import com.baggio.dscatalog.repositories.UserRepository;
 import com.baggio.dscatalog.resources.exceptions.FieldMessage;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid, UserUpdateDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
 	
 	@Autowired
 	private UserRepository userRepository;
@@ -22,13 +28,16 @@ public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid,
 	public void initialize(UserUpdateValid ann) {
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public boolean isValid(UserUpdateDTO dto, ConstraintValidatorContext context) {
+		var uri = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		long userId = Long.parseLong(uri.get("id"));
 		
 		List<FieldMessage> list = new ArrayList<>();
 		
 		User user = userRepository.findByEmail(dto.getEmail());
-		if(user != null) {
+		if((user != null) && (userId != user.getId())) {
 			list.add(new FieldMessage("email", "Este e-mail já existe." ));
 		}
 		
