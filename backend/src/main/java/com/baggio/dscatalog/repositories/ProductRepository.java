bc4d5d8bd1bcc12c167f61422e2c58e3838f6cc8
rodrigo.baggio.si@gmail.com <Baggio25>
2024-02-22 07:47:34 -0300

    SQL use case

--- backend/src/main/java/com/baggio/dscatalog/repositories/ProductRepository.java
+++ backend/src/main/java/com/baggio/dscatalog/repositories/ProductRepository.java
@@ -6,4 +6,18 @@ import com.baggio.dscatalog.entities.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long>{
 
+  /**
+   *SELECT DISTINCT
+        tb_product.id, 
+        tb_product.name
+    FROM
+        tb_product
+    INNER JOIN tb_product_category ON tb_product.id = tb_product_category.product_id
+    WHERE
+          tb_product_category.category_id IN (1,3) AND
+          LOWER(tb_product.name) LIKE '%ma%'
+    ORDER BY
+          tb_product.name
+   */
+
 }
